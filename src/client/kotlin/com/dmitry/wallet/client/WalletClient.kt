e75c42261b5933949ae3e04b1543f41d56a9c2c0
Dmitry_Strelnikov <formeazaz@gmail.com>
2026-09-04 19:41:56 +0700

    add creative tab initialization and fix initialization  of items

--- src/client/kotlin/com/dmitry/wallet/client/WalletClient.kt
+++ src/client/kotlin/com/dmitry/wallet/client/WalletClient.kt
@@ -1,11 +1,9 @@
 package com.dmitry.wallet.client
 
-import com.dmitry.wallet.ModItems
 import net.fabricmc.api.ClientModInitializer
 
 object WalletClient : ClientModInitializer {
 	override fun onInitializeClient() {
 		// This entrypoint is suitable for setting up client-specific logic, such as rendering.
-		ModItems.init()
 	}
 }
--- src/main/kotlin/com/dmitry/wallet/Wallet.kt
+++ src/main/kotlin/com/dmitry/wallet/Wallet.kt
@@ -14,7 +14,17 @@ object Wallet : ModInitializer {
 		// However, some things (like resources) may still be uninitialized.
 		// Proceed with mild caution.
 
-		LOGGER.info("Hello Fabric world!")
+		LOGGER.info("Initialising wallet mod...")
+
+		LOGGER.info("Initializing items...")
+		ModItems.init()
+		LOGGER.info("Items initialized successfully")
+
+		LOGGER.info("Initializing creative tab...")
+		CreativeTab.init()
+		LOGGER.info("Creative tab initialized successfully")
+
+		LOGGER.info("Wallet mod initialized successfully")
 	}
 
 	fun id(path: String): Identifier = Identifier(MOD_ID, path)
